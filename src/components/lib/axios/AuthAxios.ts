1c9f5bf5876cc612184bf8ad672d56fa7bbf0cbe
yoooonbong <a1056719@gmail.com>
2024-10-17 16:45:23 +0900

    💩 리프레시토큰 관련 오류

--- src/components/lib/axios/AuthAxios.ts
+++ src/components/lib/axios/AuthAxios.ts
@@ -24,32 +24,32 @@ authApi.interceptors.request.use(
   },
 );
 
-authApi.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (
-      error.response?.data?.message === 'Unauthorized' &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-      const refreshToken = Cookies.get('refreshToken');
-      if (refreshToken) {
-        try {
-          const response = await authApi.post('/auth/token', {
-            refreshToken,
-          });
-          const { accessToken } = response.data;
-          Cookies.set('accessToken', accessToken);
-          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-          return authApi(originalRequest);
-        } catch (refreshError) {
-          console.error('토큰 갱신 실패:', refreshError);
-        }
-      }
-    }
-    return Promise.reject(error);
-  },
-);
+// authApi.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (
+//       error.response?.data?.message === 'Unauthorized' &&
+//       !originalRequest._retry
+//     ) {
+//       originalRequest._retry = true;
+//       const refreshToken = Cookies.get('refreshToken');
+//       if (refreshToken) {
+//         try {
+//           const response = await authApi.post('/auth/tokens', {
+//             refreshToken,
+//           });
+//           const { accessToken } = response.data;
+//           Cookies.set('accessToken', accessToken);
+//           originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//           return authApi(originalRequest);
+//         } catch (refreshError) {
+//           console.error('토큰 갱신 실패:', refreshError);
+//         }
+//       }
+//     }
+//     return Promise.reject(error);
+//   },
+// );
 
 export default authApi;
