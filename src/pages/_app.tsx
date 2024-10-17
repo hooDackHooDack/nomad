eb0fc40394c7c58c9429b891360be4b5206d756b
yoooonbong <a1056719@gmail.com>
2024-10-17 17:48:14 +0900

    🐛 헤더컴포넌트 쿼리프로바이더 내부 렌더링 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Header from '@/components/header/Header';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Header />
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
