f8360c6420e111d77857d613e591f077c214bfc3
ddol9 <drddol9@gmail.com>
2024-11-04 02:13:05 +0900

    🏷️ 예약 api any타입 삭제

--- src/lib/api/reservation.ts
+++ src/lib/api/reservation.ts
@@ -2,7 +2,7 @@ import { ReservationsResponse } from '@/types/mypage/reservations';
 import authApi from '../axios/auth';
 
 export const fetchReservations = async (size: number, status?: string) => {
-  const params: Record<string, any> = {
+  const params: { size: number; status?: string } = {
     size,
   };
 
